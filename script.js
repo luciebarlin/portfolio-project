510258aedf0dcbf0daf891e13cc935c423149ffd
luciebarlin <114916719+luciebarlin@users.noreply.github.com>
2023-01-03 17:44:48 +0100

    fixed nav bbar issues

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 //dropdown menus
 
-const menuBtns = document.querySelectorAll(".js-menu-btn");
-const menuList = document.querySelectorAll(".js-menu-list");
+// const menuBtns = document.querySelectorAll(".js-menu-btn");
+// const menuList = document.querySelectorAll(".js-menu-list");
 
 
 
-menuBtns.forEach(menuBtnItem => {
-    //add the event listener to each clickable menu button
-    menuBtnItem.addEventListener("mouseover", function(event) {
-        //returns the data-target value
-        //const hideMenuElArr = document.querySelectorAll(".hide-menu");
+// menuBtns.forEach(menuBtnItem => {
+//     //add the event listener to each clickable menu button
+//     menuBtnItem.addEventListener("mouseover", function(event) {
+//         //returns the data-target value
+//         //const hideMenuElArr = document.querySelectorAll(".hide-menu");
         
-        const targetStr = event.target.dataset.target;
-        //returns target element from target string
-        if (!targetStr) {
-            return;
-        }
+//         const targetStr = event.target.dataset.target;
+//         //returns target element from target string
+//         if (!targetStr) {
+//             return;
+//         }
 
-        const target = document.querySelector('#' + targetStr);
-        //toggles the hide-menu class of the selected element
-        // console.log(event.target);
-        // console.log(targetStr);
-        //console.log(event.target);
-        if (target.classList.contains("hide-menu")) {
-            menuList.forEach(menuListItem => {
-                menuListItem.classList.add("hide-menu");
-            });
-            target.classList.remove("hide-menu");
-        } else {
-            target.classList.add("hide-menu");
-        }
+//         const target = document.querySelector('#' + targetStr);
+//         //toggles the hide-menu class of the selected element
+//         // console.log(event.target);
+//         // console.log(targetStr);
+//         //console.log(event.target);
+//         if (target.classList.contains("hide-menu")) {
+//             menuList.forEach(menuListItem => {
+//                 menuListItem.classList.add("hide-menu");
+//             });
+//             target.classList.remove("hide-menu");
+//         } else {
+//             target.classList.add("hide-menu");
+//         }
 
 
-    })
-});
+//     })
+// });
 
 //countdown timer
 
