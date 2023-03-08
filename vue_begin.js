00002f8cd7e3fb11fc7e04dcd73ebd2cb298a65a
luciebarlin <114916719+luciebarlin@users.noreply.github.com>
2023-03-08 18:19:46 +0100

    started vue project

---
+++ vue_begin.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        greeting: 'Hello, ',
+        yourName: 'Matty',
+        myName: 'Lucie',
+        navList: [
+            'Home',
+            'Experience ▾',
+            'Projects ▾',
+            'Skills',
+            'Contact'
+        ],
+        emoji: [
+            '😊', 
+            '😔'
+        ],
+        doggy: '🐶'
+
+    },
+})
